d5f5edde16ddab29fea2b577826b21d10b3c2ee2
oogre <vincent@ogre.be>
2021-02-17 17:37:52 +0100

    first commit

---
+++ src/Tools.js
@@ -0,0 +1,20 @@
+/*----------------------------------------*\
+  21.2.camera - Tools.js
+  @author Evrard Vincent (vincent@ogre.be)
+  @Date:   2021-02-14 18:32:12
+  @Last Modified time: 2021-02-14 19:52:00
+\*----------------------------------------*/
+
+export const divAndMod = (value, divisor) => {
+	let i = 0;
+	let rest = value
+	while(rest >= divisor){
+		rest = rest - divisor;
+		i ++;
+	}
+	return [i, rest]
+}
+
+export const isFunction = (functionToCheck) => {
+ return functionToCheck && {}.toString.call(functionToCheck) === '[object Function]';
+}
